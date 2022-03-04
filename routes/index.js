abd3d03b4903e7bbf270968e77a8c33ac6b04a76
Julia Nemell <jnemell@gmail.com>
2022-03-04 13:19:29 +0100

    added CLEARDB_DATABASE_URL

--- routes/index.js
+++ routes/index.js
@@ -6,7 +6,7 @@ const userValidationRules = require('../validation/user');
 
 /* GET / */
 router.get('/', (req, res, next) => {
-	res.send({ success: true, data: { msg: 'oh, hi' }});
+	res.send({ success: true, data: { msg: 'ive deployed my app' }});
 });
 
 router.use('/authors', require('./authors'));
